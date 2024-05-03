8f5bc1f3d68cde81ef75725d161640a9b45e51b8
Zhaklina Dobromirova <zhaklina@MacBook-Air-de-Zhaklina.local>
2024-05-03 13:19:01 +0200

    [27] - Código Users refactorizado

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 
 use App\Http\Controllers\StreamsController;
-use App\Http\Controllers\Users;
+use App\Http\Controllers\UsersController;
 use App\Http\Controllers\Topofthetops;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/analytics/users', [Users::class, 'getUser']);
+Route::get('/analytics/users',UsersController::class);
 Route::get('/analytics/streams', StreamsController::class);
 Route::get('/analytics/topsofthetops', [Topofthetops::class, 'getTopOfTheTops']);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-use App\Http\Controllers\Users;
+use App\Http\Controllers\UsersController;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use app\Http\Controllers;
 use Illuminate\Http\Response;
@@ -12,12 +12,12 @@ abstract class TestCase extends BaseTestCase
 {
     public function givenEmptyUserIdReturnError()
     {
-        $mockedTwitchUser = $this->getMockBuilder(Users::class)
+        $mockedTwitchUser = $this->getMockBuilder(UsersController::class)
             ->disableOriginalConstructor()
             ->getMock();
 
         // Inject the mocked dependency
-        $this->app->instance(Users::class, $mockedTwitchUser);
+        $this->app->instance(UsersController::class, $mockedTwitchUser);
 
         // Make a request with empty user ID
         $response = $this->get('/users?id=');
