8e884b1a8cf9dc3af275dc847428d1873c0b9477
Zhaklina Dobromirova <zhaklina@MacBook-Air-de-Zhaklina.local>
2024-05-03 13:27:05 +0200

    [25] - Añadida DS_Store a .gitignore

--- app/Services/TwitchUserService.php
+++ app/Services/TwitchUserService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use Carbon\Carbon;
 use App\Models\UsersTwitch;
 use Illuminate\Support\Facades\Http;
+use Exception;
 
 class TwitchUserService
 {
@@ -30,24 +31,25 @@ class TwitchUserService
 
     private function getDataForUserFromAPI($userID)
     {
-        $clientID = env('TWITCH_CLIENT_ID');
+        $clientID    = env('TWITCH_CLIENT_ID');
         $accessToken = $this->tokenTwitch->getToken();
 
         return Http::withHeaders([
-            'Client-ID' => $clientID,
+            'Client-ID'     => $clientID,
             'Authorization' => 'Bearer ' . $accessToken,
         ])->get("https://api.twitch.tv/helix/users?id={$userID}");
     }
 
-    private function processApiResponse($response) {
+    private function processApiResponse($response)
+    {
 
         if ($response->failed()) {
-            throw new \Exception('Failed to retrieve data from Twitch API');
+            throw new Exception('Failed to retrieve data from Twitch API');
         }
 
         $userData = $response->json()['data'][0] ?? null;
         if (!$userData) {
-            throw new \Exception('User not found on Twitch');
+            throw new Exception('User not found on Twitch');
         }
 
         return $userData;
@@ -58,15 +60,15 @@ class TwitchUserService
         return UsersTwitch::updateOrCreate(
             ['id' => $userData['id']],
             [
-                'login' => $userData['login'],
-                'display_name' => $userData['display_name'],
-                'type' => $userData['type'],
-                'broadcaster_type' => $userData['broadcaster_type'],
-                'description' => $userData['description'],
+                'login'             => $userData['login'],
+                'display_name'      => $userData['display_name'],
+                'type'              => $userData['type'],
+                'broadcaster_type'  => $userData['broadcaster_type'],
+                'description'       => $userData['description'],
                 'profile_image_url' => $userData['profile_image_url'],
                 'offline_image_url' => $userData['offline_image_url'],
-                'view_count' => $userData['view_count'],
-                'created_at' => Carbon::parse($userData['created_at'])->toDateTimeString(),
+                'view_count'        => $userData['view_count'],
+                'created_at'        => Carbon::parse($userData['created_at'])->toDateTimeString(),
             ]
         );
     }
