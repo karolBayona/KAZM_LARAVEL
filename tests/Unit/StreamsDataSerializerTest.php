2b83bda4f87a8fff69f50809a384e63d01675573
amlndz <alejandromelendez2014@gmail.com>
2024-05-08 13:04:24 +0200

    [34] - Añadir test_with_missing_data de StreamsDataSerializer

--- tests/Unit/StreamsDataSerializerTest.php
+++ tests/Unit/StreamsDataSerializerTest.php
@@ -24,6 +24,23 @@ class StreamsDataSerializerTest extends TestCase
         $this->assertEquals($expectedData, $serializedData);
     }
 
+    public function test_streams_serializable_with_missing_data()
+    {
+        $streamData = [
+            ['title' => 'Stream 1'],
+            ['user_name' => 'user2'],
+            ['title' => 'Stream 3', 'user_name' => 'user3'],
+        ];
 
+        $serializedData = StreamsDataSerializer::serialize($streamData);
+
+        $expectedData = [
+            ['title' => 'Stream 1', 'user_name' => null],
+            ['title' => null, 'user_name' => 'user2'],
+            ['title' => 'Stream 3', 'user_name' => 'user3'],
+        ];
+
+        $this->assertEquals($expectedData, $serializedData);
+    }
 
 }
