c011858db66829ce7a4ae0e29b192937aa3e1611
karolBayona <bayona.138260@e.unavarra.es>
2024-05-12 14:12:47 +0200

    [75] - Arreglado error static

--- database/migrations/2024_03_25_220042_create_top_games_table.php
+++ database/migrations/2024_03_25_220042_create_top_games_table.php
@@ -4,18 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTopGamesTable extends Migration
-{
-    public function up()
+/**
+ * Migration to create 'top_games' table.
+ *
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ */
+return new class () extends Migration {
+    /**
+     * Run the migrations to create the 'top_games' table.
+     *
+     * @return void
+     */
+    public function up(): void
     {
         Schema::create('top_games', function (Blueprint $table) {
-            $table->string('game_id')->unique();
-            $table->string('game_name'); // Nombre del juego
+            $table->string('game_id')->unique(); // Unique identifier for the game.
+            $table->string('game_name');         // Name of the game.
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations by dropping the 'top_games' table.
+     *
+     * @return void
+     */
+    public function down(): void
     {
         Schema::dropIfExists('top_games');
     }
-}
+};
