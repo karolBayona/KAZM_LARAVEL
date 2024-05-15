c3c4ca8fb73ed548506453b9924143b7ce657c99
andermonreal <117311517+andermonreal@users.noreply.github.com>
2024-05-16 00:21:22 +0200

    [80] - Creacion de controlador y ruta de la creacion de usuarios (#82)
    * [80] - Creacion de controlador y ruta de la creacion de usuarios
    
    * [80] - Arreglando fallos de la creación del controlador para nuevos usuarios
    
    * [80] - Arreglando fallos de la creación del controlador para nuevos usuarios

---
+++ app/Infrastructure/Controllers/CreateNewUserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Infrastructure\Controllers;
+
+use App\Config\JsonReturnMessages;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CreateNewUserController
+{
+    private NewUserProvider $newUserProvider;
+
+    public function __construct(NewUserProvider $newUserProvider)
+    {
+        $this->newUserProvider = $newUserProvider;
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        if (empty($username) || empty($password)) {
+            return response()->json(['error' => JsonReturnMessages::NEW_USER_PARAMETER_MISSING], 400);
+        }
+
+        $userCreated = $this->newUserProvider->createUser($username, $password);
+
+        if ($userCreated) {
+            return response()->json(['username' => $username, 'message' => JsonReturnMessages::NEW_USER_SUCCESSFUL_RESPONSE], 201);
+        }
+
+        $userExists = $this->newUserProvider->userExists($username);
+
+        if ($userExists) {
+            return response()->json(['error' => JsonReturnMessages::NEW_USER_ALREADY_EXISTS], 409);
+        }
+
+        return response()->json(['error' => JsonReturnMessages::NEW_USER_SERVER_ERROR], 500);
+    }
+}
