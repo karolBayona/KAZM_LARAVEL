8aad70ebe997227dfea9aaa21baac55657a599fc
andermonreal <andermonreal2003@gmail.com>
2024-05-16 11:38:12 +0200

    [85] - Creacion de migracion para la tabla TwitchUsers

--- app/Models/TwitchUsers.php
+++ app/Models/TwitchUsers.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class TwitchUsers extends Model
 {
-    protected $id = 'id';
+    protected $idUser   = 'idUser';
     protected $username = 'username';
     protected $password = 'password';
 }
