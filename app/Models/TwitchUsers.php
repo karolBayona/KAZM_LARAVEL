c4d7e3ce3d8b7de6c762ec0ab93843e497387d91
andermonreal <andermonreal2003@gmail.com>
2024-05-16 11:36:34 +0200

    [85] - Creacion de migracion para la tabla TwitchUsers

---
+++ app/Models/TwitchUsers.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TwitchUsers extends Model
+{
+    protected $id = 'id';
+    protected $username = 'username';
+    protected $password = 'password';
+}
---
+++ database/migrations/2024_05_16_083835_create_streamers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('streamersTwitch', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('login');
+            $table->string('display_name');
+            $table->string('type')->nullable();
+            $table->string('broadcaster_type')->nullable();
+            $table->string('description')->nullable();
+            $table->string('profile_image_url')->nullable();
+            $table->string('offline_image_url')->nullable();
+            $table->integer('view_count')->default(0);
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('streamers');
+    }
+};
