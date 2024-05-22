59c7dd375ff151ead433271635647f691371c248
andermonreal <andermonreal2003@gmail.com>
2024-05-22 18:37:44 +0200

    [93] - Controlador y ruta del listado de usuarios

---
+++ app/Infrastructure/Controllers/GetUsersListController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Infrastructure\Controllers;
+
+use App\Config\JsonReturnMessages;
+use App\Services\UsersDataManager\GetUsersListProvider;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class GetUsersListController
+{
+    private GetUsersListProvider $usersListProvider;
+
+    public function __construct(GetUsersListProvider $usersListProvider)
+    {
+        $this->$usersListProvider = $usersListProvider;
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $users = $this->usersListProvider->execute();
+
+        return response()->json($users, 200);
+    }
+}
