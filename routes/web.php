31cd135136dc8adbd5ff7c8dd034f0768cc9a483
andermonreal <117311517+andermonreal@users.noreply.github.com>
2024-05-22 18:52:56 +0200

    [93] - Controlador y ruta del listado de usuarios (#95)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Infrastructure\Controllers\CreateNewUserController;
+use App\Infrastructure\Controllers\GetUsersListController;
 use App\Infrastructure\Controllers\StreamsController;
 use App\Infrastructure\Controllers\GetStreamersController;
 use App\Infrastructure\Controllers\Topofthetops;
@@ -10,3 +11,4 @@ Route::get('/analytics/streamers', GetStreamersController::class);
 Route::get('/analytics/streams', StreamsController::class);
 Route::get('/analytics/topsofthetops', [Topofthetops::class, 'getTopOfTheTops']);
 Route::post('/analytics/users', CreateNewUserController::class);
+Route::get('/analytics/users', GetUsersListController::class);
