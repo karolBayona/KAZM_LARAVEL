72f9a58c43e5d0bceb8d9cd01949d467d15b4456
amlndz <alejandromelendez2014@gmail.com>
2024-05-29 00:16:38 +0200

    [105] - Logica followStreamersController+ test unitarios error 400

--- app/Config/JsonReturnMessages.php
+++ app/Config/JsonReturnMessages.php
@@ -11,9 +11,11 @@ class JsonReturnMessages
 
     public const USER_LIST_SERVER_ERROR_500 = 'Error del servidor al obtener la lista de usuarios.';
 
-    public const FOLLOW_STREAMER_PARAMETER_MISSING_OR_INVALID_401 = ' Token de autenticación no proporcionado o inválido';
+    public const FOLLOW_STREAMER_PARAMETER_MISSING_OR_INVALID_400 = 'Usuario o Streamer no proporcionado';
+    public const FOLLOW_STREAMER_UNAUTHORIZED_401                 = 'Token de autenticación no proporcionado o inválido';
     public const FOLLOW_STREAMER_FORBIDDEN                        = 'Acceso denegado debido a permisos insuficientes';
     public const FOLLOW_STREAMER_NOT_FOUND_404                    = 'El usuario ( userId ) o el streamer ( streamerId ) especificado no existe en la API';
     public const FOLLOW_STREAMERS_CONFLICT_409                    = 'El usuario ya está siguiendo al streamer';
     public const FOLLOW_STREAMERS_SERVER_ERROR_500                = 'Error del servidor al seguir al streamer';
+    public const FOLLOW_STREAMER_SUCCESFUL_RESPONSE_200           = 'Ahora sigues a streamerId';
 }
--- app/Infrastructure/Controllers/FollowStreamerController.php
+++ app/Infrastructure/Controllers/FollowStreamerController.php
@@ -2,11 +2,28 @@
 
 namespace App\Infrastructure\Controllers;
 
+use App\Config\JsonReturnMessages;
+use App\Services\UsersDataManager\FollowStreamersProvider;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class FollowStreamerController
 {
-    public function __invoke(Request $request)
+    private $followProvider;
+
+    public function __construct($followProvider)
     {
+        $this->followProvider = $followProvider;
+    }
+    public function __invoke(Request $request): JsonResponse
+    {
+        $user_id     = $request->input('userId');
+        $streamer_id = $request->input('streamerId');
+
+        if(empty($user_id) || empty($streamer_id)) {
+            return response()->json(['error' => JsonReturnMessages::FOLLOW_STREAMER_PARAMETER_MISSING_OR_INVALID_400], 400);
+        }
+
+        return response()->json(['error' => JsonReturnMessages::FOLLOW_STREAMERS_SERVER_ERROR_500]);
     }
 }
