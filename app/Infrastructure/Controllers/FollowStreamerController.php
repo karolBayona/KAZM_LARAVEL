cd9f9eb960ee8bcfdc7b0f82ea63ef1344166874
m3l <157792352+amlndz@users.noreply.github.com>
2024-05-28 23:01:30 +0200

    [103] - Añadir ruta seguir streamer (#104)
    * [103] - Añadir ruta seguir streamer
    
    * [103] - Añadir ruta seguir streamer
    
    ---------
    
    Co-authored-by: amlndz <“alejandromelendez2014@gmail.com”>

---
+++ app/Infrastructure/Controllers/FollowStreamerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Infrastructure\Controllers;
+
+use Illuminate\Http\Request;
+
+class FollowStreamerController
+{
+    public function __invoke(Request $request)
+    {
+    }
+}
