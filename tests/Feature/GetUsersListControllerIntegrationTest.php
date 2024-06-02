1c480997d9a68dc1e8572cfc9b3b6731ca480d83
andermonreal <117311517+andermonreal@users.noreply.github.com>
2024-06-02 23:03:25 +0200

    [151] - Cubrimiento con test de integracion para el listado de usuarios (#154)

---
+++ tests/Feature/GetUsersListControllerIntegrationTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Config\JsonReturnMessages;
+use App\Infrastructure\Clients\DBClient;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mockery;
+use Tests\TestCase;
+use Exception;
+
+/**
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ */
+class GetUsersListControllerIntegrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $dbClientMock;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dbClientMock = Mockery::mock(DBClient::class);
+        $this->app->instance(DBClient::class, $this->dbClientMock);
+    }
+
+    /** @test */
+    public function it_returns_users_list_successfully()
+    {
+        $usersList = [
+            [
+                'username' => 'user1',
+                'followedStreamers' => '[1, 2, 3]'
+            ],
+            [
+                'username' => 'user2',
+                'followedStreamers' => '[4, 5, 6]'
+            ]
+        ];
+
+        $this->dbClientMock->expects('getAllTwitchUsers')
+            ->once()
+            ->andReturn($usersList);
+
+        $response = $this->getJson('/analytics/users');
+
+        $response->assertStatus(200);
+        $response->assertJson($usersList);
+    }
+
+    /** @test */
+    public function it_returns_server_error_on_exception()
+    {
+        $this->dbClientMock->expects('getAllTwitchUsers')
+            ->once()
+            ->andThrow(new Exception());
+
+        $response = $this->getJson('/analytics/users');
+
+        $response->assertStatus(500);
+        $response->assertJson([
+            'Internal Server Error' => JsonReturnMessages::USER_LIST_SERVER_ERROR_500,
+        ]);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+}
